787a22cfa4b87da736b07d64596bc6657c32c446
MarkedOlive <olivdev4@gmail.com>
2022-12-16 20:01:40 -0400

    Comenzando las actualizaciones de es8; Entendiendo el metodo object.entries

---
+++ ECMAScript/src/es8/00-object-entries.js
@@ -0,0 +1,5 @@
+// El metodo object entries devuelve una matriz de pares propios
+
+const countries = { MX: 'Mexico', VZ: 'Venezuela', CA: 'Canada', CO: 'Colombia' }
+
+console.log(Object.entries(countries))
